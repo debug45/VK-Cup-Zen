5b6b37c73bb8ca8b05264e8ab0b99645af27a771
Sergey Moskvin <s@mosk.vin>
2023-01-09 16:46:54 +0400

    Prepare the project

---
+++ Zen/Framework/Instances.swift
@@ -0,0 +1,22 @@
+//
+//  Instances.swift
+//  Zen
+//
+//  Created by Sergey Moskvin on 09.01.2023.
+//
+
+import UIKit
+
+final class Instances {
+    
+    static let shared = Instances()
+    
+    let mainWindow = UIWindow()
+    let rootNavigationController = UINavigationController()
+    
+    private init() {
+        mainWindow.rootViewController = rootNavigationController
+        rootNavigationController.isNavigationBarHidden = true
+    }
+    
+}
---
+++ Zen/Product Scenes/MainViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MainViewController.swift
+//  Zen
+//
+//  Created by Sergey Moskvin on 09.01.2023.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    
+}
