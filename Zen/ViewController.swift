a1a316545f157a42daf1133bc729d057cb5a098c
Sergey Moskvin <s@mosk.vin>
2023-01-09 16:12:49 +0400

    Create the project

---
+++ Zen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Zen
+//
+//  Created by Sergey Moskvin on 09.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
